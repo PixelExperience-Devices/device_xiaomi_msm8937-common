84bc3b183dc380f4f04e762535be05a6e2a8fdcf
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    msm8937-common: Remove obsolete dex settings
    This block is completely redundant because:
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_PIC is never read
    - DEX_PREOPT_DEFAULT := nostripping has no effect when the
      BOARD_USES_SYSTEM_OTHER_ODEX flag is set to true
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,16 +90,6 @@ BOARD_USES_QCNE := true
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-
 # Display
 TARGET_SCREEN_DENSITY := 290
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
