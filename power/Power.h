4ff886463653ace95155a405ec2ea6f7dd4d61fa
Nich <nctrenco@gmail.com>
2019-03-04 15:41:43 +0800

    msm8937-common: power: Upgrade to power HAL 1.1 API
    Support async power hint and subsystem low power stats.
    
    Change-Id: I3edd40b7434c6108d19c2cb3d1c077621eff999e

--- power/Power.h
+++ power/Power.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,10 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_POWER_V1_0_POWER_H
-#define ANDROID_HARDWARE_POWER_V1_0_POWER_H
+#ifndef ANDROID_HARDWARE_POWER_V1_1_POWER_H
+#define ANDROID_HARDWARE_POWER_V1_1_POWER_H
 
-#include <android/hardware/power/1.0/IPower.h>
+#include <android/hardware/power/1.1/IPower.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 #include <hardware/power.h>
@@ -26,12 +26,12 @@
 namespace android {
 namespace hardware {
 namespace power {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
 using ::android::hardware::power::V1_0::Feature;
 using ::android::hardware::power::V1_0::PowerHint;
-using ::android::hardware::power::V1_0::IPower;
+using ::android::hardware::power::V1_1::IPower;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
 
@@ -45,15 +45,18 @@ struct Power : public IPower {
     Return<void> setFeature(Feature feature, bool activate) override;
     Return<void> getPlatformLowPowerStats(getPlatformLowPowerStats_cb _hidl_cb) override;
 
+    // Methods from ::android::hardware::power::V1_1::IPower follow
+    Return<void> getSubsystemLowPowerStats(getSubsystemLowPowerStats_cb _hidl_cb) override;
+    Return<void> powerHintAsync(PowerHint hint, int32_t data) override;
 
     // Methods from ::android::hidl::base::V1_0::IBase follow.
 
 };
 
 }  // namespace implementation
-}  // namespace V1_0/1
+}  // namespace V1_1
 }  // namespace power
 }  // namespace hardware
 }  // namespace android
 
-#endif  // ANDROID_HARDWARE_POWER_V1_0_POWER_H
+#endif  // ANDROID_HARDWARE_POWER_V1_1_POWER_H
