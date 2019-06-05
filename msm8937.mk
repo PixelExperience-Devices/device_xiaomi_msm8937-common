a5f23d789f3573d4c4f6210399bd1d3d32f88627
ErrorNetwork28 <zidan23.05.2016@gmail.com>
2019-06-05 09:05:13 +0300

    Drop Snap Camera

--- msm8937.mk
+++ msm8937.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
