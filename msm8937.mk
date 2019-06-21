4caabc4684eb73a30c49b4367278786aa23b5f60
silent27121984 <sergey27121984@gmail.com>
2019-06-21 17:34:41 +0300

    ThermalController app

--- msm8937.mk
+++ msm8937.mk
@@ -367,6 +367,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8937
 
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
