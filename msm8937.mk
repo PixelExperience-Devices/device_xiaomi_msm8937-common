6e1a944c5d0e897e574aab2510c6fbe3844d64d9
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    msm8937-common: Switch to Audio HAL 6.0
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -58,9 +58,9 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \
