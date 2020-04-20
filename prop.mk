8cf88e23ed421f4ee7d06b4b9ba4a85d378608f5
Redick Lin <redicklin@google.com>
2020-04-20 18:28:53 +0800

    msm8937-common: BOARD_CHARGER_ENABLE_SUSPEND becomes ro.charger.enable_suspend
    Define ro.charger.enable_suspend to true as charger no
    longer uses BOARD_CHARGER_ENABLE_SUSPEND.
    
    Bug: 153937817
    Change-Id: Ic76bf3a66c5e21c74e49607f54d6c0358c31bbac
    Signed-off-by: Redick Lin <redicklin@google.com>
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- prop.mk
+++ prop.mk
@@ -65,6 +65,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qti.telephony.vt_cam_interface=1 \
     vidc.enc.dcvs.extra-buff-count=2
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
