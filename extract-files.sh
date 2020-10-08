291be4af9f1e6fdef342f7434c17eb0c95e5ea9e
zqm64 <zqm64@outlook.com>
2020-10-08 17:18:55 +0800

    msm8937-common: extract-files: Remove device-specific stuff

--- extract-files.sh
+++ extract-files.sh
@@ -75,8 +75,4 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt
             "${KANG}" --section "${SECTION}"
 fi
 
-# Hax for cam configs
-CAMERA2_SENSOR_MODULES="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
-sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
-
 "$MY_DIR"/setup-makefiles.sh
