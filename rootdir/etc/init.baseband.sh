743491f8b1a4bc9769d1026c2fdb52de6717c3b5
zqm64 <zqm64@outlook.com>
2020-11-12 11:22:37 +0800

    msm8937-common: Ensure baseband prop won't overwritten by ril daemon init

--- rootdir/etc/init.baseband.sh
+++ rootdir/etc/init.baseband.sh
@@ -1,5 +1,8 @@
 #!/vendor/bin/sh
 
+# FIXME: ril-daemon always resets baseband prop, so wait for it to reset first
+sleep 5s
+
 baseband_str=$(strings /vendor/firmware_mnt/image/modem.b12 | grep "QC_IMAGE_VERSION_STRING=" | cut -c 25-)
 
 if [ ! -z $baseband_str ]; then
