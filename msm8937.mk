78a070ffc5d75f6be0d157805c5205283bfdc9f9
SakilMondal <mondalsakil@gmail.com>
2020-11-21 22:51:50 +0100

    msm8937-common: Build QTI thermal HAL
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -373,6 +373,9 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
