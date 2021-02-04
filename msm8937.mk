jro1979oliver/device_motorola_msm8937-common

msm8937-common: Use component-override to configure Bluetooth
Runtime Resource Overlays (RROs) can no longer change the value of
resources in manifest when read during PackagerParser.

This change uses component-override to enable the service by default.

Remove usages of profile_supported_* from overlays as they no
longer configure the enable state of Bluetooth components.

Bug: 135048762
Test: adb shell pm query-services \
      com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
      adb shell pm query-services \
      com.android.bluetooth/.hid.HidDeviceService
      adb shell pm query-services \
      com.android.bluetooth/.sap.SapService

Change-Id: I284d254a921b14d1533745611a083f4b5a27d9dc

msm8937.mk
@@ -80,6 +80,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8937 \

PixelExperience-Devices/device_xiaomi_msm8937-common

Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

msm8937.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 	$(COMMON_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8937 \